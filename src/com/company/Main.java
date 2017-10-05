5d4332ede9965ac94dd2874176d54b9744817e07
Alex Bao <xin.bao.alex@gmail.com>
2017-10-05 19:51:30 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class Main {
+
+    //create properties value and create& write into xyz.properties text file
+    public static void main2 (String[] args) throws Exception
+    {
+        Properties props = new Properties();
+
+        props.put("displayName", "Jim");
+        props.put("stageName", "Jimmy");
+        props.put("realName", "James");
+        String name = props.getProperty("realName");
+
+        try (Writer writer = Files.newBufferedWriter(Paths.get("xyz.properties"))){
+            props.store(writer,"My Comment2");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Properties prop = new Properties();
+
+        try(Reader reader = Files.newBufferedReader(Paths.get("myapp.properties"))){
+            prop.load(reader);
+        }
+
+        String val1 = prop.getProperty("val1");
+        String val2 = prop.getProperty("val2");
+        String val3 = prop.getProperty("val3");
+        String val4 = prop.getProperty("val4");
+
+        System.out.println(val1);
+        System.out.println(val2);
+        System.out.println(val3);
+        System.out.println(val4);
+        
+    }
+
+}
